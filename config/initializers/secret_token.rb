a15d1555de0b31b2518a24eacdb17b4bf5d5c34e
robinsk1 <robinskilling@gmail.com>
2013-10-12 14:09:39 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Versus::Application.config.secret_token = 'fdcd106d1bc3a3d1394730ce6041b83404d326d6cdf54ebb8c128b32d9b5bbf95a57edd28da49661ca1cc0cd691d996c915fba0ccbf5efcb00e0f7edfcd51881'
